a6584bca929d49b2fdd2d15b20e8e8d53152ff4f
KesvaL <jabeddou@gmail.com>
2023-06-19 18:42:55 +0200

    💄 Navigation Styling on mobile

--- src/lib/components/brand/Logo.svelte
+++ src/lib/components/brand/Logo.svelte
@@ -16,19 +16,16 @@
 	class:repeat
 >
 	<svg
-		class="h-full max-h-7 w-auto"
+		class="flex h-full max-h-7 w-auto justify-center text-center"
 		width="820"
 		height="116"
 		viewBox="0 0 723.84009 116.720269"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<text
-			xml:space="preserve"
-			transform="translate(-300,-206)"
-			class="fill-base-content transition-all duration-300"
-			><tspan x="275" y="292" id="tspan1319"
-				><tspan style="font-size:107px;font-family:'Benzin Semibold',serif;" id="tspan1317">
+		<text transform="translate(-300,-206)" class="fill-base-content transition-all duration-300"
+			><tspan x="275" y="292"
+				><tspan style="font-size:107px;font-family:'benz',serif;">
 					{common.appName}
 				</tspan></tspan
 			></text
--- src/lib/components/layout/NavigationLink.svelte
+++ src/lib/components/layout/NavigationLink.svelte
@@ -8,7 +8,7 @@
 <a
 	{href}
 	class="{active
-		? 'active !border-primary bg-primary text-primary-content max-md:bg-base-200/30 md:border-r-2'
+		? 'active !border-primary bg-primary max-md:bg-primary/5 md:border-r-2 md:text-primary-content'
 		: 'max-md:bg-base-100'} md:flex md:items-center md:gap-4 md:px-8 md:py-2 md:text-lg"
 >
 	<slot name="icon" />

289608ddd206691b7d4bdf493a887ddb40cea092
KesvaL <jabeddou@gmail.com>
2023-06-20 15:18:08 +0200

    💄 Update Food Card

--- src/lib/components/cards/HomeFoodCard.svelte
+++ src/lib/components/cards/HomeFoodCard.svelte
@@ -6,11 +6,13 @@
 
 <a
 	href="/profile"
-	class="btn-ghost btn flex h-auto w-full flex-nowrap justify-between bg-base-300 px-4 py-4 normal-case shadow-lg"
+	class="btn-ghost btn flex h-auto w-full flex-nowrap justify-between bg-base-200 px-4 py-4 normal-case shadow-lg"
 >
 	<div class="flex flex-col justify-start">
-		<h2 class="w-fit text-base">Food</h2>
-		<p><span class="text-3xl">0</span><span>/{$userData.calorieGoal}</span> Cal</p>
+		<h2 class="w-fit text-lg">Food</h2>
+		<p class="font-light">
+			<span class="text-3xl">0</span><span>/{$userData.calorieGoal}</span> Cal
+		</p>
 	</div>
 	<button class="btn-primary btn px-8">Enter</button>
 </a>
